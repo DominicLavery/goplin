348cd414fdb2bd87b38cd9dde4d667a11cb9962e
Dominic Lavery <che@thisdevice.co.uk>
2020-10-14 18:20:01 +0100

    Make the reading pane bigger

--- go.mod
+++ go.mod
@@ -7,4 +7,6 @@ require (
 	github.com/gdamore/tcell v1.4.0
 	github.com/rivo/tview v0.0.0-20200915114512-42866ecf6ca6
 	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/sys v0.0.0-20201014080544-cc95f250f6bc // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- ui/AppUI.go
+++ ui/AppUI.go
@@ -6,6 +6,13 @@ import (
 	"github.com/rivo/tview"
 )
 
+// Enum for screens & focus
+const (
+	NotebookTree = iota
+	NoteTree
+	NoteView
+)
+
 func MakeApp(notebooks []models.Notebook, notes []models.Note) *tview.Application {
 	app := tview.NewApplication()
 
@@ -20,12 +27,14 @@ func MakeApp(notebooks []models.Notebook, notes []models.Note) *tview.Applicatio
 		noteView,
 	}
 
-	flex := tview.NewFlex()
+	flex := tview.NewFlex().SetDirection(tview.FlexRow)
 	renderFlex := func() {
 		flex.Clear()
-		for i, display := range displays {
-			flex.AddItem(display, 0, 1, i == focused)
-		}
+		flex.AddItem(tview.NewFlex().
+			AddItem(notebookTree, 0, 1, focused == NotebookTree).
+			AddItem(notesTree, 0, 1, focused == NoteTree).
+			AddItem(noteView, 0, 2, focused == NoteView), // Twice as big
+		0, 1, true)
 		app.SetFocus(flex)
 	}
 
