e49710c4efae9799e88de43a1833ce4e269111bc
Dominic Lavery <che@thisdevice.co.uk>
2020-11-30 11:32:58 +0000

    Add some initial testing to the filesystem source

---
+++ data/FilesystemSource_test.go
@@ -0,0 +1,143 @@
+package data
+
+import (
+	"dominiclavery/goplin/models"
+	"github.com/spf13/afero"
+	"testing"
+)
+
+func TestNewFilesystemSourceDeepDirectory_flat(t *testing.T) {
+	//arrange
+	afs := afero.NewMemMapFs()
+	_ = afs.Mkdir("/root", 0644)
+	_ = afs.Mkdir("/root/book1", 0644)
+	_ = afs.Mkdir("/root/book2", 0644)
+	_ = afs.Mkdir("/root/book3", 0644)
+	_ = afs.Mkdir("/root/.ignored", 0644)
+
+	expectedBooks := models.Notebook{
+		Name:     "root",
+		Id:       0,
+		ParentId: -1,
+		Children: []models.Notebook{
+			{
+				Id:       1,
+				ParentId: 0,
+				Name:     "book1",
+			},
+			{
+				Id:       2,
+				ParentId: 0,
+				Name:     "book2",
+			},
+			{
+				Id:       3,
+				ParentId: 0,
+				Name:     "book3",
+			},
+		},
+	}
+
+	//act
+	newFilesystemSource("/root", &afero.Afero{Fs: afs})
+
+	//assert
+	if notebooks.highestNotebookId != 4 {
+		t.Errorf("Filesystem did not find the correct amount of notebooks. Expected %d, got %d", 4, notebooks.highestNotebookId)
+	}
+	assertBookIsLike(expectedBooks, notebooks.notebookRoot,t)
+}
+
+func TestNewFilesystemSourceDeepDirectory_deep(t *testing.T) {
+	//arrange
+	afs := afero.NewMemMapFs()
+	_ = afs.Mkdir("/root", 0644)
+	_ = afs.Mkdir("/root/book1", 0644)
+	_ = afs.Mkdir("/root/book1/book2", 0644)
+	_ = afs.Mkdir("/root/book1/book2/book3", 0644)
+	expectedBooks := models.Notebook{
+		Name:     "root",
+		Id:       0,
+		ParentId: -1,
+		Children: []models.Notebook{{
+			Id:       1,
+			ParentId: 0,
+			Name:     "book1",
+			Children: []models.Notebook{{
+				Id:       2,
+				ParentId: 1,
+				Name:     "book2",
+				Children: []models.Notebook{{
+					Id:       3,
+					ParentId: 2,
+					Name:     "book3",
+				}},
+			}},
+		}},
+	}
+
+	//act
+	newFilesystemSource("/root", &afero.Afero{Fs: afs})
+
+	//assert
+	if notebooks.highestNotebookId != 4 {
+		t.Errorf("Filesystem did not find the correct amount of notebooks. Expected %d, got %d", 4, notebooks.highestNotebookId)
+	}
+
+	assertBookIsLike(expectedBooks, notebooks.notebookRoot, t)
+}
+
+func TestFilesystemWriter_MakeBook(t *testing.T) {
+	//arrange
+	afs := afero.NewMemMapFs()
+	_ = afs.Mkdir("/root", 0644)
+
+	fs := FilesystemWriter{
+		rootPath: "/root",
+		fs:       &afero.Afero{Fs: afs},
+	}
+	notebooks.notebookRoot.Name = "root"
+	notebooks.notebookRoot.Path = "/root"
+	expectedBook := models.Notebook{
+		Id:       1,
+		ParentId: 0,
+		Name:     "new",
+		Children: nil,
+	}
+
+	//act
+	go func() {
+		_ = fs.MakeBook("new")
+	}()
+	notebooks := <-NotebooksChan
+
+	//assert
+	if len(notebooks.Children) != 1 {
+		t.Errorf("Expected root book to have 1 children. Had %d children", len(notebooks.Children))
+	}
+	assertBookIsLike(expectedBook, notebooks.Children[0], t)
+	//TODO test for dir existence once relative pathing is correctly set up (and therefore, the location of the dir is predictable)
+}
+
+func assertBookIsLike(expected models.Notebook, actual models.Notebook, t *testing.T) {
+	if expected.Name != actual.Name {
+		t.Errorf("Expected created book to have name [%s]. Had [%s]", expected.Name, actual.Name)
+	}
+	if expected.Id != actual.Id {
+		t.Errorf("Expected created book to have ID [%d]. Had [%d]", expected.Id, actual.Id)
+	}
+	if expected.ParentId != actual.ParentId {
+		t.Errorf("Expected created book to have ParentId [%d]. Had [%d]", expected.ParentId, actual.ParentId)
+	}
+	if expected.Children != nil {
+		if actual.Children == nil {
+			t.Errorf("Expected created book to have [%d] childrem. Had 0", len(expected.Children))
+		}
+		if len(expected.Children) != len(actual.Children) {
+			t.Errorf("Expected created book to have [%d] childrem. Had [%d]", len(expected.Children), len(actual.Children))
+		}
+		for i, book := range expected.Children {
+			assertBookIsLike(book, actual.Children[i], t)
+		}
+	}
+}
